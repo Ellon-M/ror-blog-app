ec2f6bce423326778890542256fba17ccb80f66d
Ellon M <mordecaimwema11@gmail.com>
2023-03-31 16:12:15 +0300

    create associations between tables and perform CRUD operations

--- db/migrate/20230329205252_create_posts.rb
+++ db/migrate/20230329205252_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :title
       t.string :text
       t.string :bio
-      t.references :author, null: false, foreign_key: { to_table: :users }
       t.integer :comments_counter
       t.integer :likes_counter
       t.timestamps
