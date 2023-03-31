6304ffd9ac8edbbcc193dc9c6a4dbd68ee9ef054
Ellon M <mordecaimwema11@gmail.com>
2023-03-31 16:11:27 +0300

    create models for each entity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
-class Post < ApplicationRecord
+class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
+
+  def recent_posts
+    posts.limit(3).order(created_at: :desc)
+  end
 end
