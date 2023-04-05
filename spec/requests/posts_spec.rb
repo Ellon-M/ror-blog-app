24bc3697b9fbd2d92f6c1face140f0df37355c00
Ellon M <mordecaimwema11@gmail.com>
2023-04-05 14:08:42 +0300

    Fix rubocop errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
-describe "/posts", type: :request do
-
-  describe "GET /index" do
-    before (:example) do
+describe '/posts', type: :request do
+  describe 'GET /index' do
+    before(:example) do
       get '/users/1/posts'
     end
-    it "has a successful response" do
+    it 'has a successful response' do
       expect(response).to be_successful
     end
-    it "correctly renders the index html template" do
+    it 'correctly renders the index html template' do
       expect(response).to render_template(:index)
     end
-    it "contains the placeholder text" do
-      expect(response.body).to include("Here is a list of posts for a specific user")
+    it 'contains the placeholder text' do
+      expect(response.body).to include('Here is a list of posts for a specific user')
     end
   end
 
-  describe "GET /show" do
-    before (:example) do
+  describe 'GET /show' do
+    before(:example) do
       get '/users/1/posts/1'
     end
-    it "renders a successful response" do
+    it 'renders a successful response' do
       expect(response).to be_successful
     end
-    it "correctly renders the show template" do
+    it 'correctly renders the show template' do
       expect(response).to render_template(:show)
     end
-    it "contain the placeholder text" do
-      expect(response.body).to include("Here is a post for a given user")
+    it 'contain the placeholder text' do
+      expect(response.body).to include('Here is a post for a given user')
     end
   end
 end
