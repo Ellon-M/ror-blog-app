e4e7e8bdff1378e309bd415d117cd52c75f11427
Alex Ogeto <alexogeto54@gmail.com>
2023-04-11 22:52:34 +0300

    Modify app/controllers/comments_controller.rb file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
   end
-
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
@@ -11,4 +11,10 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to user_post_path(@user, @post)
   end
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    flash[:notice] = "The Comment was successfully deleted."
+    redirect_to user_post_url(User.find(params[:user_id]), Post.find(params[:post_id]))
+   end
 end
