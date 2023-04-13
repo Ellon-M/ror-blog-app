0b7a410049aa0159adac33ee7d58fc0bb8bee49b
Alex Ogeto <alexogeto54@gmail.com>
2023-04-13 19:39:04 +0300

    Update Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
-  belongs_to :post, foreign_key: :post_id
-
+  belongs_to :post, foreign_key: :post_id, counter_cache: :comments_counter
   after_save :update_comments_counter
-
   def update_comments_counter
-    post.increment!(:comments_counter)
+    if post.comments_counter.nil?
+      post.update(comments_counter: 1)
+    else
+      post.comments_counter += 1
+      post.update(comments_counter: post.comments_counter)
+    end
   end
 end
